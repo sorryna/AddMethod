03aaabc0f17829b85ab909b565ee2e30fd0730be
sikarin <so.sikarin@perfenterprise.com>
2020-08-04 16:47:43 +0700

    add new project

---
+++ AddMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
